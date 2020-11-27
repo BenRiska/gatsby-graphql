14659030a05d30adeca20a33e2c2eca9e8b626bb
bentaylor <ben10taylor10@hotmail.com>
2020-11-27 18:03:49 +0000

    templating done

---
+++ src/data/MenuData.js
@@ -0,0 +1,5 @@
+export const menuData = [
+    {title: "About",link: "/about"},
+    {title: "Trips",link: "/trips"},
+    {title: "Careers",link: "/careers"},
+    {title: "Contact",link: "/contact"},]
